4a2d0c0f8db1c1d3708852bbfae5d86c2e74b208
kibreabg <kibreabg@gmail.com>
2020-02-27 11:14:51 +0200

    Search by Worksheet Serial No bug fixed

--- users/getworksheets.php
+++ users/getworksheets.php
@@ -1,19 +1,18 @@
 <?php
 session_start();
-	include("../connection/config.php");
+include "../connection/config.php";
 $q = strtolower($_GET["q"]);
-$labss=$_SESSION['lab'];
-if (!$q) return;
-
-$sql = "select worksheets.ID, worksheets.worksheetno  FROM worksheets
-					WHERE worksheets.ID LIKE '$q%' AND worksheets.lab='$labss' ";
-$rsd = mysql_query($sql)or die(mysql_error());
-while($rs = mysql_fetch_array($rsd)) {
-	$cid = $rs['ID'];
-	$cname = $rs['ID'] ;
-	echo "$cname|$cid\n";
-	
-	
-	
+$labss = $_SESSION['lab'];
+if (!$q) {
+    return;
 }
-?>
+
+$sql = "SELECT worksheets.worksheetno, worksheets.ID FROM worksheets
+					WHERE (worksheets.ID LIKE '%$q%' OR worksheets.worksheetno LIKE '%$q%') AND worksheets.lab='$labss' ";
+$rsd = mysql_query($sql) or die(mysql_error());
+while ($rs = mysql_fetch_array($rsd)) {
+    $cid = $rs['ID'];
+    $cname = $rs['worksheetno'];
+    echo "$cname|$cid\n";
+
+}
